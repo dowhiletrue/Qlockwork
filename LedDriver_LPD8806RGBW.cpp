dd11614f0ecfbb0982f6f3b9971b72644ffb9d8d
Michael Zaugg <zaugg@puzzle.ch>
2021-02-13 14:28:23 +0100

    original source code 20190901 from bracci.ch

---
+++ LedDriver_LPD8806RGBW.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+ LedDriver_LPD8806RGBW.cpp
+ ******************************************************************************/
+
+#include "LedDriver_LPD8806RGBW.h"
+
+LedDriver_LPD8806RGBW::LedDriver_LPD8806RGBW(eLayout layout) : LedDriverBase(layout) {
+	m_numLeds = getLedsCount(layout);
+	strip = new LPD8806RGBW(m_numLeds, PIN_LEDS_DATA, PIN_LEDS_CLOCK);
+	strip->begin();
+}
+
+String LedDriver_LPD8806RGBW::getSignature() {
+	return "LPD8806RGBW";
+}
+
+void LedDriver_LPD8806RGBW::clear() {
+	for (uint8_t i = 0; i < m_numLeds; i++)
+		strip->setPixelColor(i, 0);
+}
+
+void LedDriver_LPD8806RGBW::show() {
+	strip->show();
+}
+
+void LedDriver_LPD8806RGBW::setPixel(uint8_t num, uint32_t color) {
+  uint32_t c = ((((color)&0xFF)/2) << 16)|((((color >> 16)&0xFF)/2) << 8)|((((color >> 8)&0xFF)/2));
+	strip->setPixelColor(num, c);
+}
